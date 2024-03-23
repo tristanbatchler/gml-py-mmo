9eebdecc8f920c837c05d4ed236a05aa3de0a7ac
Tristan Batchler <batchlertristan@gmail.com>
2024-03-24 09:23:41 +1000

    Add network client variable and movement input handling to actor.gd

--- godot_client/actor.gd
+++ godot_client/actor.gd
@@ -5,6 +5,7 @@ const SPEED: float = 450.0
 const EPS: float = 1.0
 
 var next_pos: Vector2 = Vector2.ZERO
+@export var network_client: NetworkClient
 
 func _ready():
 	next_pos = position
@@ -12,14 +13,29 @@ func _ready():
 func _input(event):
 	if next_pos != position:
 		return	# Don't accept movement input until we are aligned
+		
+	var move_dir: Vector2 = Vector2.ZERO
 	if event.is_action_pressed("move_right"):
-		next_pos.x += GRID
+		move_dir.x = GRID
 	elif event.is_action_pressed("move_left"):
-		next_pos.x -= GRID
+		move_dir.x = -GRID
 	elif event.is_action_pressed("move_down"):
-		next_pos.y += GRID
+		move_dir.y = GRID
 	elif event.is_action_pressed("move_up"):
-		next_pos.y -= GRID
+		move_dir.y = -GRID
+		
+	if move_dir == Vector2.ZERO:
+		return
+		
+	next_pos += move_dir
+		
+	#network_client.send_packet({
+		#"move": {
+			#"from_pid": null  # Need to figure out how to pass PID to player
+			#"dx": move_dir.x,
+			#"dy": move_dir.y
+		#}
+	#})
 	
 func _physics_process(delta):
 	if next_pos != position:
